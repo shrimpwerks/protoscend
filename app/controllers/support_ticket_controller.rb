f3c9952b6b297a4441fd33a5cf64ed42fab5835e
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-08-07 01:19:24 -0700

    adds nothing relevant 😢

--- app/controllers/support_ticket_controller.rb
+++ app/controllers/support_ticket_controller.rb
@@ -1,2 +1,9 @@
 class SupportTicketController < ApplicationController
+  protect_from_forgery with: :exception
+  respond_to :json
+  
+  def show
+    @ticket = SupportTicket.find(params[:id])
+    render json: @ticket.to_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :user, :only => [:show]
+  resources :support_ticket, :only => [:show]
 end
