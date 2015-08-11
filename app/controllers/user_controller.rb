7b30aebfbc4ef09a1b0f0496f106aa0747a6d36f
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-08-10 20:20:06 -0700

    adds user searching

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,6 +10,12 @@ class UserController < ApplicationController
   def index
     @users_count = User.count
     @users = User.list_users(params)
+    @users = @users.offset_by(params) if params[:offset].present?
+    @users = @users.order_by(params) if params[:sort_by].present?
+    @users = @users.search_fname(params[:s_fname]) if params[:s_fname].present?
+    @users = @users.search_lname(params[:s_lname]) if params[:s_lname].present?
+    @users = @users.search_email(params[:s_email]) if params[:s_email].present?
+
 
     render json: @users, meta: { count: @users_count }
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
+
   # associations
   belongs_to :small_group, inverse_of: :users
+
+  # alias_method "fuck_name", "fname"
 
   has_many :assigned_routes, inverse_of: :user
   has_many :comments, inverse_of: :user
@@ -10,11 +13,28 @@ class User < ActiveRecord::Base
   has_many :support_tickets, inverse_of: :user
 
   def self.list_users(params)
-    joins('left outer join routes on routes.user_id = users.id')
-    .group(:id)
+    joins('left outer join routes on routes.user_id = users.id').group(:id)
     .select('users.*, count(routes.id) as route_count')
     .limit(5)
-    .offset(params[:offset])
-    .order(params[:sort_by].to_s + ' ' + params[:order_by].to_s)
+  end
+
+  def self.search_fname(fname)
+    where("users.fname LIKE :fname", {:fname => "%#{fname}%"})
+  end
+
+  def self.search_lname(lname)
+    where("users.lname LIKE :lname", {:lname => "%#{lname}%"})
+  end
+
+  def self.search_email(email)
+    where("users.email LIKE :email", {:email => "%#{email}%"})
+  end
+
+  def self.offset_by(params)
+    offset(params[:offset])
+  end
+
+  def self.order_by(params)
+    order(params[:sort_by].to_s + ' ' + params[:order_by].to_s)
   end
 end
