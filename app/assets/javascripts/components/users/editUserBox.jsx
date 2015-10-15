ac95a8e46ef0146fcd8ac0c230ed6795dfc74e17
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-10-15 08:56:14 -0700

    adds non working code, but i have to commit for a sec

--- app/assets/javascripts/components/users/editUserBox.jsx
+++ app/assets/javascripts/components/users/editUserBox.jsx
@@ -3,11 +3,8 @@ var EditUserBox = React.createClass({
     this.loadUsersFromServer();
   },
   getInitialState: function() {
-    var url = window.location.href;
-    var prof_id = url.split('/user/')[1];
-
     return {
-      url: 'http://localhost:3000/api/user/' + prof_id,
+      url: 'http://localhost:3000/api/user/' + this.props.id,
       user_level: sessionStorage.getItem('user_level'),
       user_id: sessionStorage.getItem('id'),
       auth_token: sessionStorage.getItem('authentication'),
@@ -22,15 +19,23 @@ var EditUserBox = React.createClass({
       dataType: 'json',
       cache: false,
       success: function(data) {
-        // this.setState({
-        //
-        // });
-        console.log(data);
+        this.setState({
+          edit_id: data.id,
+          edit_email: data.email,
+          edit_fname: data.fname,
+          edit_lname: data.lname,
+          edit_user_level: data.user_level,
+          edit_small_group: data.small_group
+        });
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
+  },
+  handleChange: function(e) {
+    console.log(e);
+    this.setState({edit_email: e.target.value});
   },
   render: function() {
     return (
@@ -39,19 +44,25 @@ var EditUserBox = React.createClass({
           <div className="col-lg-8 col-md-8 col-xs-12">
             <div className="page-header">
               <h1>
-                {(this.state.user_level < 4) ? 'View Profile' : 'Edit Profile'}
+                {
+                  (this.state.user_level >= 4 || this.state.user_id === this.state.edit_id) ?
+                    'Edit Profile' : 'View Profile'
+                }
                 <br />
-                <small>temporary email holder</small>
+                <small>{this.state.edit_email}</small>
               </h1>
             </div>
-            {this.state.url}
             <table className="table">
               <tr>
                 <th>
                   Email
                 </th>
                 <td>
-                  temporary email holder
+                  {
+                    (this.state.user_level >= 4 || this.state.user_id === this.state.edit_id) ?
+                      <input type="text" name="edit_user_email" className="form-control" value={this.state.edit_email} onChange={this.handleChange} /> :
+                      (this.state.edit_email)
+                  }
                 </td>
               </tr>
             </table>
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,13 @@ class UserController < ApplicationController
   # before_action :authenticate_with_token!
 
   def show
+    @user_id = params[:id]
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user_id = params[:id]
+    @user = User.find(params[:id])
   end
 
   def index
