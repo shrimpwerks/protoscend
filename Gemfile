f00524434d783127e771fbadbd9b04db04cfa6fa
Jonah George <jonah.george@me.com>
2015-10-31 11:09:29 -0700

    Rewrite Users#index without React

--- Gemfile
+++ Gemfile
@@ -25,7 +25,6 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
 gem 'active_model_serializers'
 
 # for working with react
@@ -35,7 +34,6 @@ gem 'react-rails', '1.2'
 # gem 'sabisu_rails', github: "IcaliaLabs/sabisu-rails"
 # gem 'compass-rails'
 # gem 'furatto'
-# gem 'font-awesome-rails'
 # gem 'simple_form'
 
 group :development do
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,26 @@
 class UserController < ApplicationController
-  # protect_from_forgery with: :exception
-  # before_action :authenticate_with_token!
+  helper_method :sort_column, :sort_direction
+
+  def index
+    @users = User.where(nil)
+    @users = @users.with_first_name(params[:first_name]) if params[:first_name].present?
+    @users = @users.with_last_name(params[:last_name]) if params[:last_name].present?
+    @users = @users.with_email(params[:email]) if params[:email].present?
+    @users = @users.order(sort_column + " " + sort_direction) 
+  end
 
   def show
   end
 
-  def index
+  def new
   end
 
-  def new
+  private
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "id"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
   end
 end
