49a29aced96b15f8c9a3824eca1216d98791d0b1
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-11-05 09:18:34 -0800

    fixes the merge i messed up

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,23 +1,12 @@
 class UserController < ApplicationController
   helper_method :sort_column, :sort_direction
 
-<<<<<<< HEAD
-  def show
-    @user_id = params[:id]
-    @user = User.find(params[:id])
-  end
-
-  def edit
-    @user_id = params[:id]
-    @user = User.find(params[:id])
-=======
   def index
     @users = User.where(nil)
     @users = @users.with_first_name(params[:first_name]) if params[:first_name].present?
     @users = @users.with_last_name(params[:last_name]) if params[:last_name].present?
     @users = @users.with_email(params[:email]) if params[:email].present?
-    @users = @users.order(sort_column + " " + sort_direction) 
->>>>>>> f2db2045b9019ed802e328dc1836284b6faad20e
+    @users = @users.order(sort_column + " " + sort_direction)
   end
 
   def show
@@ -30,7 +19,7 @@ class UserController < ApplicationController
   def sort_column
     User.column_names.include?(params[:sort]) ? params[:sort] : "id"
   end
-  
+
   def sort_direction
     %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
   end
