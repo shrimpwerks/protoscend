188944f9306125d3045dbae793daca911f28772e
Jonah George <jonah.george@me.com>
2015-11-07 13:49:12 -0800

    Remove dead code in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ActiveRecord::Base
-  # validates :auth_token, uniqueness: true
-  # validates :email, uniqueness: true
   validates :fname, presence: true
   validates :lname, presence: true
-  # validates :password, length: { in: 8..20 }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  # before_create :generate_authentication_token!
 
   belongs_to :small_group, inverse_of: :users
   has_many :assigned_routes, inverse_of: :user
@@ -17,12 +12,6 @@ class User < ActiveRecord::Base
   has_many :ratings, inverse_of: :user
   has_many :routes, inverse_of: :user
   has_many :support_tickets, inverse_of: :user
-
-  # def generate_authentication_token!
-  #   begin
-  #     self.auth_token = Devise.friendly_token
-  #   end while self.class.exists?(auth_token: auth_token)
-  # end
 
   def self.with_first_name(first_name)
     where("users.fname LIKE ?", "%#{first_name}%")
