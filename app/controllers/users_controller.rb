9a4c0782cd76e71e3e12bbabe2f9562201e0a452
Jonah George <jonah.george@me.com>
2016-03-28 08:38:32 -0700

    Set default UsersController#index sorting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,10 +52,10 @@ class UsersController < ApplicationController
   end
 
   def sort_column
-    User.column_names.include?(params[:sort]) ? params[:sort] : "id"
+    User.column_names.include?(params[:sort]) ? params[:sort] : "first_name"
   end
 
   def sort_direction
-    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+    %w(asc desc).include?(params[:direction]) ?  params[:direction] : "asc"
   end
 end
