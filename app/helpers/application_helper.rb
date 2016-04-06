abaaaab6b47fb843fcefb1273a7b793f872eba99
Jonah George <jonah.george@me.com>
2016-04-06 15:45:14 -0700

    Fix maintenance requests table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,5 @@
 module ApplicationHelper
 
-  # def sortable(column, title = nil)
-  #   title ||= column.titleize
-  #   direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-  #   if (column == sort_column)
-  #     glyph_class = (sort_direction == "asc") ? "glyphicon-triangle-top" : "glyphicon-triangle-bottom" 
-  #     title = title + content_tag(:span, nil, class: "glyphicon #{glyph_class} pull-right")
-  #   end
-  #   link_to raw(title), params.merge({sort: column, direction: direction})
-  # end
-
   def sortable_header(column, title = nil)
     title ||= column.titleize
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
