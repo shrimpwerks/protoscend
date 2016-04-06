c3cd609bf2ec7d0779a3810180f4d205f4f13ce6
Jonah George <jonah.george@me.com>
2016-04-06 15:31:52 -0700

    More improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,26 @@
 module ApplicationHelper
 
-  def sortable(column, title = nil)
+  # def sortable(column, title = nil)
+  #   title ||= column.titleize
+  #   direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+  #   if (column == sort_column)
+  #     glyph_class = (sort_direction == "asc") ? "glyphicon-triangle-top" : "glyphicon-triangle-bottom" 
+  #     title = title + content_tag(:span, nil, class: "glyphicon #{glyph_class} pull-right")
+  #   end
+  #   link_to raw(title), params.merge({sort: column, direction: direction})
+  # end
+
+  def sortable_header(column, title = nil)
     title ||= column.titleize
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
+    header_class = "header"
     if (column == sort_column)
-      glyph_class = (sort_direction == "asc") ? "glyphicon-triangle-top" : "glyphicon-triangle-bottom" 
-      title = title + content_tag(:span, nil, class: "glyphicon #{glyph_class} pull-right")
+      header_class += (sort_direction == "asc") ? " header-sort-up" : " header-sort-down" 
     end
-    link_to raw(title), params.merge({sort: column, direction: direction})
+    link = link_to title, params.merge({sort: column, direction: direction})
+    content_tag(:th, link, class: header_class)
   end
+
 
   def form_errors_for(object=nil)
     render('shared/form_errors', object: object) unless object.blank?
