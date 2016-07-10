bdbf244d715987f5a99740f55ba0974441aab271
Ty Skelton <skeltont@onid.oregonstate.edu>
2016-07-10 14:15:47 -0700

    changes policies to inherit application init and adhere to user - record structure for each action. (#110)

--- app/policies/maintenance_request_policy.rb
+++ app/policies/maintenance_request_policy.rb
@@ -1,49 +1,43 @@
 class MaintenanceRequestPolicy < ApplicationPolicy
-  attr_reader :request, :record
-
-  def initialize(current_user, request)
-    @current_user = current_user
-    @request = request
-  end
 
   def index?
-    @current_user.role != "Public"
+    @user.role != "Public"
   end
 
   def show?
-    @current_user.role != "Public"
+    @user.role != "Public"
   end
 
   def new?
-    @current_user.role != "Public"
+    @user.role != "Public"
   end
 
   def create?
-    @current_user.role != "Public"
+    @user.role != "Public"
   end
 
   def edit?
     # Supervisors and up, or the owner of the request
-    (@current_user.role != "Public" and @current_user.role != "Setter" and
-     @current_user.role != "Employee") or
-     @current_user.id == @request.user_id
+    (@user.role != "Public" and @user.role != "Setter" and
+     @user.role != "Employee") or
+     @user.id == @record.user_id
   end
 
   def update?
     # Supervisors and up, or the owner of the request
-    (@current_user.role != "Public" and @current_user.role != "Setter" and
-     @current_user.role != "Employee") or
-     @current_user.id == @request.user_id
+    (@user.role != "Public" and @user.role != "Setter" and
+     @user.role != "Employee") or
+     @user.id == @record.user_id
   end
 
   def resolve?
-    @current_user.role != "Public"
+    @user.role != "Public"
   end
 
   def destroy?
     # Supervisors and up, or the owner of the request
-    (@current_user.role != "Public" and @current_user.role != "Setter" and
-     @current_user.role != "Employee") or
-     @current_user.id == @request.user_id
+    (@user.role != "Public" and @user.role != "Setter" and
+     @user.role != "Employee") or
+     @user.id == @record.user_id
   end
 end
--- app/policies/rating_policy.rb
+++ app/policies/rating_policy.rb
@@ -1,8 +1,3 @@
 class RatingPolicy < ApplicationPolicy
-  attr_reader :rating, :record
 
-  def initialize(current_user, rating)
-    @current_user = current_user
-    @rating = rating
-  end
 end
