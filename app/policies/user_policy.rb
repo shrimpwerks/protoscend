60676b9506e7bec3214be35f14bf7cfb40562b5f
Ty Skelton <skeltont@onid.oregonstate.edu>
2016-07-26 09:46:22 -0700

    fixes displaying disabled routes in graphs, and perms for editing users (#137)

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -3,13 +3,15 @@ class UserPolicy < ApplicationPolicy
     @user.role != "Public"
   end
 
+  ## managers and admins can edit anyone, supervisors can edit anyone that's not
+  ## an admin or a supervisor.
   def edit?
     @user.role == "Manager" || @user.role == "Administrator" ||
-    @user.role == "Supervisor"
+    (@user.role == "Supervisor" && (@record.role != "Manager" && @record.role != "Administrator"))
   end
 
   def update?
     @user.role == "Manager" || @user.role == "Administrator" ||
-    @user.role == "Supervisor"
+    (@user.role == "Supervisor" && (@record.role != "Manager" && @record.role != "Administrator"))
   end
 end
