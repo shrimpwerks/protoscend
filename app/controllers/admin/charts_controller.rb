95f5696320027b2ebb5c47af98fadfcec75e8fdf
Ty Skelton <skeltont@oregonstate.edu>
2016-11-20 20:47:26 -0800

    adds non working chart stuff

--- app/controllers/admin/charts_controller.rb
+++ app/controllers/admin/charts_controller.rb
@@ -2,28 +2,31 @@ module Admin
   class ChartsController < ApplicationController
 
     def routes
-      render json: [
+      butts = [
         { name: "Active", data: active_routes },
         { name: "Expired", data: expired_routes },
         { name: "Assigned", data: assigned_routes }
       ]
+      puts Route.live_routes.location(params[:location]).group(:grade).order(:grade).count
+      puts butts
+      render json: butts
     end
 
     private
 
     def active_routes
-      Route.live_routes.location(params[:location]).group(:grade).count
-        .map { |key, value| [Route.grades.select { |gkey, gvalue| gvalue == key }.keys.first, value] }
+      Route.live_routes.location(params[:location]).group(:grade).order(:grade).count
+        .map { |key, value| [Route.grades.each { |gkey, gvalue| gvalue == key }.keys[key], value] }
     end
 
     def expired_routes
-      Route.active_routes.expired_routes.location(params[:location]).group(:grade).count
-        .map { |key, value| [Route.grades.select { |gkey, gvalue| gvalue == key }.keys.first, value] }
+      Route.active_routes.expired_routes.location(params[:location]).group(:grade).order(:grade).count
+        .map { |key, value| [Route.grades.each { |gkey, gvalue| gvalue == key }.keys[key], value] }
     end
 
     def assigned_routes
-      Route.assigned_routes.location(params[:location]).group(:grade).count
-        .map { |key, value| [Route.grades.select { |gkey, gvalue| gvalue == key }.keys.first, value] }
+      Route.assigned_routes.location(params[:location]).group(:grade).order(:grade).count
+        .map { |key, value| [Route.grades.each { |gkey, gvalue| gvalue == key }.keys[key], value] }
     end
 
   end
