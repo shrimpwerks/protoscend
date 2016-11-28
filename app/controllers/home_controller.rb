7dbde84935e0fff3ea84751ff49aca86911e9c1e
Ty Skelton <skeltont@onid.oregonstate.edu>
2016-11-27 20:07:42 -0800

    Show existing routes on user page (#194)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @announcements = Announcement.not_expired.chronological
+    @announcements = Announcement.not_expired.recent_first
     @newest_routes = Route.newest_routes
     @top_routes = Route.top_routes
   end
