eb3af6db02c2c08dd14d08863894274061ea677e
Ty Skelton <skeltont@oregonstate.edu>
2016-11-27 17:41:39 -0800

    announcements weren't ordering correctly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @announcements = Announcement.not_expired
+    @announcements = Announcement.not_expired.chronological
     @newest_routes = Route.newest_routes
     @top_routes = Route.top_routes
   end
